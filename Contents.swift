39303ae4877c83a634890b387689acb174d6a384
PegahSmiley26 <PegahSmiley@gmail.com>
2019-07-09 15:18:02 -0500

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,60 @@
+////import UIKit
+////
+////var num1 = 1
+////var num2 = 2
+////var num3 = 3
+////
+//////true or false
+////print(num1<num2)
+//////prints true
+////print(num1>=num2)
+//////prints false
+//////double equal means exact
+////print(num1==num2)
+//////prints false
+////
+////// ! means not
+//////have to put the space
+////print(num1 != num2)
+//////printed true
+////
+////print(num1 + num1)
+//////printed 2
+////
+////// & means and
+//////
+////////check is 1+2 = 3
+//////if num1 + num2 == num3 && num2+num1 == num3{
+//////    //then do something
+//////    print ("yes!")
+//////}
+////
+//////checking if 1 equals 2, then checking if 1 = 1
+////if num1 == num2  || num1 == num1 {
+////    print("yes!")
+////}
+//////doesnt print anything when it's wrong
+////
+//var dogAge = 1
+//if dogAge < 2 {
+//    print("you are a puppy")
+//}
+//if dogAge > 10 {
+//    print("Old doggie")
+//}
+//else if dogAge < 5 {
+//        print("Young Puppy")
+//    }
+//
+//var FavFood = "Starbucks"
+//FavFood < "Panda Express"
+//
+//if "Boba" == FavFood {
+//    print("BobaWasted")
+//}
+//else if "Panda Express" > FavFood {
+//    print("🤭")
+//}
+//
+//
+
